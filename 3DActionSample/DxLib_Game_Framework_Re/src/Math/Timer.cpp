89d2477a3372957a07570aa0935275ab8252e4cc
HoSiuKiG016A1351 <walker418@hotmail.com>
2019-04-21 19:30:23 +0900

    タイマークラスを作成

---
+++ 3DActionSample/DxLib_Game_Framework_Re/src/Math/Timer.cpp
@@ -0,0 +1,37 @@
+#include "Timer.h"
+#include <algorithm>
+
+// クラス：タイマー（DeltaTime式）
+// 製作者：何 兆祺（"Jacky" Ho Siu Ki）
+
+// コンストラクタ（制限時間を設定する）
+Timer::Timer(float limit_time)
+{
+	time_limit_ = limit_time;
+}
+
+// 更新
+void Timer::update(float delta_time)
+{
+	time_current_ = std::min(time_current_ + delta_time, time_limit_);
+}
+
+// 現在の時間を取得
+float Timer::get_time() const
+{
+	return time_current_;
+}
+
+// リセット
+void Timer::reset()
+{
+	time_current_ = 0.0f;
+}
+
+// タイムアウトか
+bool Timer::is_time_out() const
+{
+	if (time_limit_ <= 0.0f) return false;	// 制限時間を設定されていない場合、falseを返す
+
+	return time_current_ >= time_limit_;
+}
---
+++ 3DActionSample/DxLib_Game_Framework_Re/src/Math/Timer.h
@@ -0,0 +1,28 @@
+#ifndef TIMER_H_
+#define TIMER_H_
+
+// クラス：タイマー（DeltaTime式）
+// 製作者：何 兆祺（"Jacky" Ho Siu Ki）
+
+class Timer
+{
+public:
+	// デフォルトコンストラクタ（制限時間を設定しない）
+	Timer() = default;
+	// コンストラクタ（制限時間を設定する）
+	Timer(float limit_time);
+	// 更新
+	void update(float delta_time);
+	// 現在の時間を取得
+	float get_time() const;
+	// リセット
+	void reset();
+	// タイムアウトか
+	bool is_time_out() const;
+
+private:
+	float time_current_{ 0.0f };	// 現在の時間
+	float time_limit_{ 0.0f };		// 制限時間
+};
+
+#endif // !TIMER_H_
